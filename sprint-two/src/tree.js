ba55b1b13118fbe6822c767173adcdb021e628a2
shawnhr <shawnk723@live.com>
2017-12-22 17:14:59 -0800

    Completed tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,15 @@
+//var extend = require('extend');
 var Tree = function(value) {
+  //instead of node.next, tree store children in array
   var newTree = {};
   newTree.value = value;
-
+  
   // your code here
-  newTree.children = null;  // fix me
+  //add all child to children array
+  newTree.children = [];  // fix me
+  //functional-shared style
+  //extend(newTree, treeMethods);
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,12 +17,42 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  //Instantiate new child without using 'new' keyword
+  //push new child to children array	
+  var child = Tree(value);
+  this.children.push(child);
+  console.log(this);
+  return child;
 };
 
 treeMethods.contains = function(target) {
+  //create inner function
+  //loop through each child of tree
+  //check if any child matches with target
+
+  var contains = false;
+
+  var search = function(target, tree) {
+    //base
+    if (tree.value === target) {
+      contains = true;
+    }
+    //recurs
+    if (tree.children) {
+      tree.children.forEach( (child) => search(target, child) )
+    }
+  };
+
+  search(target, this);
+  return contains;
 };
 
-
+// var tree = Tree(1);
+// var branch1 = tree.addChild(2);
+// var branch2 = tree.addChild(3);
+// branch1.addChild(5);
+// branch2.addChild(7).addChild(8);
+// console.log(tree);
 
 /*
  * Complexity: What is the time complexity of the above functions?

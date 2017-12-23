d24cecbc6c7b2e69763b720e0fdc7ed5eb3d78ec
shawnhr <shawnk723@live.com>
2017-12-22 17:13:19 -0800

    Completed linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,68 @@
+
 var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value) {
+    //create new node
+    //if there is no head
+      //set new node as head
+    //if there is tail
+      //add new node to that tail
+    //else
+      //set new node as tail
+    var newNode = Node(value);
+
+    if(!list.head) {
+      list.head = newNode;
+    }
+    if(list.tail) {
+      list.tail.next = newNode;
+    }
+    list.tail = newNode;
   };
 
+
   list.removeHead = function() {
+    //if head is null
+      //return null;
+    //store former head in temp var
+    //set head.next as head
+    //return removed head
+
+    //if list.head === null
+      //return null
+    //else
+      //removedHead = list.head
+      //list.head = removedHead.next
+      //return removedHead.value
+    if (!list.head) {
+      return null;
+    } else {
+      var removedHead = list.head;
+      list.head = removedHead.next;
+      return removedHead.value;
+    }
   };
 
   list.contains = function(target) {
+    //loop through list from head to tail
+      //if there is any node matechs with target
+        //return true;
+      //else
+        //move to next node
+      //return flase if there is no match
+      
+    var node = list.head;
+    while(node) {
+      if (node.value === target) {
+        return true;
+      } else {
+        node = node.next;
+      }
+    }
+    return false;
   };
 
   return list;
@@ -27,3 +80,4 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
